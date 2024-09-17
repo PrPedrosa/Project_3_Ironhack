cd94b025393de95ad4feae10a4c74d92c082617c
Pedro Pedrosa <prpedrosa95@gmail.com>
2024-09-17 17:13:43 +0100

    trigger redeploy2

--- src/pages/NewFisheryForm.jsx
+++ src/pages/NewFisheryForm.jsx
@@ -1,187 +1,246 @@
-import {useState, useEffect, useContext} from 'react'
+import { useState, useEffect, useContext } from "react"
 import axios from "axios"
-import {AuthContext} from '../contexts/auth.context';
-import {useNavigate} from "react-router-dom"
+import { AuthContext } from "../contexts/auth.context"
+import { useNavigate } from "react-router-dom"
 import loadingGif from "../images/loading-gif.gif"
 function NewFisheryForm() {
-    const {user} = useContext(AuthContext)
-    const storedToken = localStorage.getItem("authToken")
+  const { user } = useContext(AuthContext)
+  const storedToken = localStorage.getItem("authToken")
 
-    const [allFishes, setAllFishes] = useState(null)
-    const [date, setDate] = useState("")
-    const [location, setLocation] = useState("")
-    const [image, setImage] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [overallWeight, setOverallWeight] = useState(0)
-    const [fishingType, setFishingType] = useState("")
-    const [fishesCaught, setFishesCaught] = useState([])
-    const [fishCaught, setFishCaught] = useState("")
-    const [weight, setWeight] = useState(0)
-    const [length, setLength] = useState(0)
+  const [allFishes, setAllFishes] = useState(null)
+  const [date, setDate] = useState("")
+  const [location, setLocation] = useState("")
+  const [image, setImage] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [overallWeight, setOverallWeight] = useState(0)
+  const [fishingType, setFishingType] = useState("")
+  const [fishesCaught, setFishesCaught] = useState([])
+  const [fishCaught, setFishCaught] = useState("")
+  const [weight, setWeight] = useState(0)
+  const [length, setLength] = useState(0)
 
-    const handleDate = (e) => setDate(e.target.value);
-    const handleLocation = (e) => setLocation(e.target.value);
-    const handleOverallWeight = (e) => setOverallWeight(+(e.target.value));
-    const handleFishingType = (e) => setFishingType(e.target.value);
-    const handleFishCaught = (e) => setFishCaught(e.target.value)
-    const handleWeight = (e) => setWeight(+(e.target.value))
-    const handleLength = (e) => setLength(+(e.target.value))
+  const handleDate = e => setDate(e.target.value)
+  const handleLocation = e => setLocation(e.target.value)
+  const handleOverallWeight = e => setOverallWeight(+e.target.value)
+  const handleFishingType = e => setFishingType(e.target.value)
+  const handleFishCaught = e => setFishCaught(e.target.value)
+  const handleWeight = e => setWeight(+e.target.value)
+  const handleLength = e => setLength(+e.target.value)
 
-    //handle image
+  //handle image
 
-    const handleUpload = async (e) => {
-        try {
-          setLoading(true);
-    
-          //formData === enctype=multipart/formdata
-          const uploadData = new FormData();
-    
-          //add the file to the formData
-          uploadData.append('image', e.target.files[0]);
-    
-          //send the file to our api
-          const response = await axios.post(`${process.env.REACT_APP_API_URL}/upload`, uploadData);
-    
-          console.log(response.data.fileUrl);
-          setImage(response.data.fileUrl);
-          setLoading(false);
-        } catch (error) {
-          console.log(error);
-          setLoading(false);
-        }
-    };
+  const handleUpload = async e => {
+    try {
+      setLoading(true)
 
-    //creates a fish object to accomodate fishery model in backend and pushes it to fishes caught state
+      //formData === enctype=multipart/formdata
+      const uploadData = new FormData()
 
-    const handleFishesCaught = () => {
-        const aCatch = {
-            species: fishCaught,
-            weight: weight,
-            length: length
-        }
-        setFishesCaught((prev) => [...prev, aCatch])
-        console.log(fishesCaught)
+      //add the file to the formData
+      uploadData.append("image", e.target.files[0])
+
+      //send the file to our api
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/upload`, uploadData)
+
+      console.log(response.data.fileUrl)
+      setImage(response.data.fileUrl)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
     }
+  }
 
-    //delete a fish object
+  //creates a fish object to accomodate fishery model in backend and pushes it to fishes caught state
 
-    const deleteFishCaught = (index) => {
-        const filteredFishes = fishesCaught.filter(fish => fishesCaught.indexOf(fish) !== index)
-        setFishesCaught(filteredFishes)
+  const handleFishesCaught = () => {
+    const aCatch = {
+      species: fishCaught,
+      weight: weight,
+      length: length,
     }
+    setFishesCaught(prev => [...prev, aCatch])
+  }
 
-    //get fishes from backend
+  //delete a fish object
 
-    const getFishes = async () => {
-        try {
-            const response = await axios.get(`${process.env.REACT_APP_API_URL}/fishes`)
-            setAllFishes(response.data);
-        } catch (error) {
-            console.log(error)
+  const deleteFishCaught = index => {
+    const filteredFishes = fishesCaught.filter(fish => fishesCaught.indexOf(fish) !== index)
+    setFishesCaught(filteredFishes)
+  }
+
+  //get fishes from backend
+
+  const getFishes = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/fishes`)
+      setAllFishes(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getFishes()
+  }, [])
+
+  //TODO handlesubmit to the backend
+  const navigate = useNavigate()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const newFishery = await axios.post(
+        `${process.env.REACT_APP_API_URL}/fisheries`,
+        { date, location, overallWeight, fishingType, fishes: fishesCaught, image, userId: user._id },
+        {
+          headers: { Authorization: `Bearer ${storedToken}` },
         }
-    }
-    useEffect(() => {getFishes()}, [])
+      )
+      console.log(newFishery)
 
-    //TODO handlesubmit to the backend
-    const navigate = useNavigate()
+      setDate("")
+      setLocation("")
+      setOverallWeight(0)
+      setFishingType("")
+      setFishesCaught([])
+      setFishCaught("")
+      setWeight(0)
+      setLength(0)
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const newFishery = await axios.post(`${process.env.REACT_APP_API_URL}/fisheries`, {date, location, overallWeight, fishingType, fishes: fishesCaught, image, userId: user._id}, {
-                headers: { Authorization: `Bearer ${storedToken}` },
-              })
-            console.log(newFishery)
-            
-            setDate("")
-            setLocation("")
-            setOverallWeight(0)
-            setFishingType("")
-            setFishesCaught([])
-            setFishCaught("")
-            setWeight(0)
-            setLength(0)
-            
-            navigate("/profile")
-        } catch (error) {
-            console.log(error)
-        }
+      navigate("/profile")
+    } catch (error) {
+      console.log(error)
     }
-    
+  }
+
   return (
     <div className='form-box form-div'>
-        <h1>Criar nova Pesca</h1>
-        <form onSubmit={handleSubmit} className='form-box form-form'>
-            <label htmlFor="date">Data* </label>
-            <input type="date" name="date" id="date" value={date} onChange={handleDate} required/>
+      <h1>Criar nova Pesca</h1>
+      <form onSubmit={handleSubmit} className='form-box form-form'>
+        <label htmlFor='date'>Data* </label>
+        <input type='date' name='date' id='date' value={date} onChange={handleDate} required />
 
-            <label htmlFor="location">Localização*</label>
-            <select name="location" id="location" onChange={handleLocation} value={location ? location : setLocation("Em frente a praia da Parede")} required>
-                <option value="Em frente a praia da Parede">Em frente à praia da Parede</option>
-                <option value="Em frente a praia das Avencas">Em frente à praia das Avencas</option>
-                <option value="Em frente a praia de Sao Pedro">Em frente à praia de São Pedro</option>
-                <option value="Em frente a praia da Bafureira">Em frente à praia da Bafureira</option>
-            </select>
+        <label htmlFor='location'>Localização*</label>
+        <select
+          name='location'
+          id='location'
+          onChange={handleLocation}
+          value={location ? location : setLocation("Em frente a praia da Parede")}
+          required
+        >
+          <option value='Em frente a praia da Parede'>Em frente à praia da Parede</option>
+          <option value='Em frente a praia das Avencas'>Em frente à praia das Avencas</option>
+          <option value='Em frente a praia de Sao Pedro'>Em frente à praia de São Pedro</option>
+          <option value='Em frente a praia da Bafureira'>Em frente à praia da Bafureira</option>
+        </select>
 
-            <label htmlFor="fishingType">Modalidade*</label>
-            <select name="fishingType" id="fishingType" onChange={handleFishingType} value={fishingType? fishingType : setFishingType("Caça submarina")} required>
-                <option value="Caça submarina">Caça submarina</option>
-                <option value="Pesca com linha">Pesca com linha</option>
-            </select>
+        <label htmlFor='fishingType'>Modalidade*</label>
+        <select
+          name='fishingType'
+          id='fishingType'
+          onChange={handleFishingType}
+          value={fishingType ? fishingType : setFishingType("Caça submarina")}
+          required
+        >
+          <option value='Caça submarina'>Caça submarina</option>
+          <option value='Pesca com linha'>Pesca com linha</option>
+        </select>
 
-            {/* <label htmlFor="image">Foto:</label>
+        {/* <label htmlFor="image">Foto:</label>
             {image && <img src={image} alt="current"/>}
             <input type="file" name="image" onChange={handleUpload} />*/}
 
-            <label htmlFor="image" className='form-box'>
-              <p>Imagem</p>
-              {image ? 
-                <>
-                  <img src={image} alt="current"/>
-                  <p className='small-buttons'>Alterar foto</p>
-                </>
-                : <i className="fa fa-3x fa-camera"><p>Adicionar Imagem</p></i>}
-                <input type="file" name='image' id='image' onChange={handleUpload} className="image-input"/>
-            </label>
+        <label htmlFor='image' className='form-box'>
+          <p>Imagem</p>
+          {image ? (
+            <>
+              <img src={image} alt='current' />
+              <p className='small-buttons'>Alterar foto</p>
+            </>
+          ) : (
+            <i className='fa fa-3x fa-camera'>
+              <p>Adicionar Imagem</p>
+            </i>
+          )}
+          <input type='file' name='image' id='image' onChange={handleUpload} className='image-input' />
+        </label>
 
-            <label htmlFor="overallWeight">Peso Total (em Kg)</label>
-            <input type="number" step="0.01" min="0" max="1000" placeholder='1.5' value={overallWeight} onChange={handleOverallWeight}/>
-            <hr style={{width: "80%"}}/>
+        <label htmlFor='overallWeight'>Peso Total (em Kg)</label>
+        <input
+          type='number'
+          step='0.01'
+          min='0'
+          max='1000'
+          placeholder='1.5'
+          value={overallWeight}
+          onChange={handleOverallWeight}
+        />
+        <hr style={{ width: "80%" }} />
 
-            <em style={{textAlign: "center"}}>Adicione em baixo todas as capturas</em>
+        <em style={{ textAlign: "center" }}>Adicione em baixo todas as capturas</em>
 
-            <label htmlFor="fishes"><b>Espécie*</b></label>
-            <select name="fishes" id="fishes" onClick={handleFishCaught} required>
-                {allFishes && allFishes.map(fish =>{
-                    return (
-                        <option value={fish.commonName} key={fish._id}>{fish.commonName}</option>
-                    )
-                })}
-            </select>
-            <label htmlFor="length">Tamanho (em cm)* </label>
-            <input type="number" name="length" id="length" onChange={handleLength} required/>
+        <label htmlFor='fishes'>
+          <b>Espécie*</b>
+        </label>
+        <select name='fishes' id='fishes' onClick={handleFishCaught} required>
+          {allFishes &&
+            allFishes.map(fish => {
+              return (
+                <option value={fish.commonName} key={fish._id}>
+                  {fish.commonName}
+                </option>
+              )
+            })}
+        </select>
+        <label htmlFor='length'>Tamanho (em cm)* </label>
+        <input type='number' name='length' id='length' onChange={handleLength} required />
 
-            <label htmlFor="weight">Peso (em kg)* </label>
-            <input type="number" name="weight" id="weight" step="0.01" min="0" max="1000" placeholder='1.5' onChange={handleWeight} required/>
-            <span onClick={handleFishesCaught} className='small-buttons'>Adicionar Captura</span>
+        <label htmlFor='weight'>Peso (em kg)* </label>
+        <input
+          type='number'
+          name='weight'
+          id='weight'
+          step='0.01'
+          min='0'
+          max='1000'
+          placeholder='1.5'
+          onChange={handleWeight}
+          required
+        />
+        <span onClick={handleFishesCaught} className='small-buttons'>
+          Adicionar Captura
+        </span>
 
-            {/* loop over fishescaught array to show to user and let him delete */}
-            <div className='fishes-box'>
-            {fishesCaught.map(fish => {
-                return (
-                    <div key={fish._id} className="display-fishes">
-                        <span>{fish.species}</span>
-                        <span>{fish.length} cm</span>
-                        <span> {fish.weight} kg</span>
-                        <button onClick={() => {deleteFishCaught(fishesCaught.indexOf(fish))}} className="small-buttons logout">delete</button>
-                    </div>
-                )
-            })}      
-            </div>
+        {/* loop over fishescaught array to show to user and let him delete */}
+        <div className='fishes-box'>
+          {fishesCaught.map(fish => {
+            return (
+              <div key={fish._id} className='display-fishes'>
+                <span>{fish.species}</span>
+                <span>{fish.length} cm</span>
+                <span> {fish.weight} kg</span>
+                <button
+                  onClick={() => {
+                    deleteFishCaught(fishesCaught.indexOf(fish))
+                  }}
+                  className='small-buttons logout'
+                >
+                  delete
+                </button>
+              </div>
+            )
+          })}
+        </div>
 
-            {!loading ? <button type="submit" className='buttons'>Submeter</button> : <img src={loadingGif} alt="loading" className='loading-gif'/>}
-        </form>
-        <em>Campos marcados com * são obrigatórios</em>
+        {!loading ? (
+          <button type='submit' className='buttons'>
+            Submeter
+          </button>
+        ) : (
+          <img src={loadingGif} alt='loading' className='loading-gif' />
+        )}
+      </form>
+      <em>Campos marcados com * são obrigatórios</em>
     </div>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,124 +1,152 @@
-import { AuthContext } from '../contexts/auth.context';
-import {useContext, useState} from "react"
-import axios from 'axios';
-import Fishery from '../components/Fishery';
-import { Link } from 'react-router-dom';
+import { AuthContext } from "../contexts/auth.context"
+import { useContext, useState } from "react"
+import axios from "axios"
+import Fishery from "../components/Fishery"
+import { Link } from "react-router-dom"
 import loadingGif from "../images/loading-gif.gif"
 
 function Profile() {
-    const {user} = useContext(AuthContext)
-    const storedToken = localStorage.getItem("authToken")
-    
-    const [allUserFisheries, setAllUserFisheries] = useState(null)
-    const [allUserFishes, setAllUserFishes] = useState(null)
-    const [seeFisheries, setSeeFisheries] = useState(false)
-    const [seeFishes, setSeeFishes] = useState(false)
-    const [loading, setLoading] = useState(false)
-    console.log(seeFishes)
-    console.log(allUserFishes)
+  const { user } = useContext(AuthContext)
+  const storedToken = localStorage.getItem("authToken")
 
-    const toggleSeeFisheries = () => {
-      if(allUserFisheries){
-        setSeeFisheries(false)
-        setAllUserFisheries(null)
-      } else {
-        setLoading(true)
-        getFisheries()
-        setSeeFisheries(true)
-        setLoading(false)
-      }
-    }
+  const [allUserFisheries, setAllUserFisheries] = useState(null)
+  const [allUserFishes, setAllUserFishes] = useState(null)
+  const [seeFisheries, setSeeFisheries] = useState(false)
+  const [seeFishes, setSeeFishes] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-    const toggleSeeFishes = () => {
-      if (allUserFishes) {
-        setSeeFishes(false)
-        setAllUserFishes(null);
-      } else {
-        setLoading(true)
-        getFishes()
-        setSeeFishes(true)
-        setLoading(false)
-      }
+  const toggleSeeFisheries = () => {
+    if (allUserFisheries) {
+      setSeeFisheries(false)
+      setAllUserFisheries(null)
+    } else {
+      setLoading(true)
+      getFisheries()
+      setSeeFisheries(true)
+      setLoading(false)
     }
-    
-    const getFisheries = async () => {
-        try {
-            const response = await axios.get(`${process.env.REACT_APP_API_URL}/fisheries`, {
-              headers: { Authorization: `Bearer ${storedToken}` },
-            })
-            setAllUserFisheries(response.data.filter(fishery => fishery.userId === user._id))
-        } catch (error) {
-            console.log(error)
-        }
+  }
+
+  const toggleSeeFishes = () => {
+    if (allUserFishes) {
+      setSeeFishes(false)
+      setAllUserFishes(null)
+    } else {
+      setLoading(true)
+      getFishes()
+      setSeeFishes(true)
+      setLoading(false)
     }
-    const getFishes = async () => {
-        try {
-            const response = await axios.get(`${process.env.REACT_APP_API_URL}/userfishes`, {
-              headers: { Authorization: `Bearer ${storedToken}` },
-            })
-            console.log(response.data)
-            setAllUserFishes(response.data.filter(fish => fish.userId._id === user._id))
-        } catch (error) {
-            console.log(error)
-        }
+  }
+
+  const getFisheries = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/fisheries`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      setAllUserFisheries(response.data.filter(fishery => fishery.userId === user._id))
+    } catch (error) {
+      console.log(error)
     }
+  }
+  const getFishes = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/userfishes`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      console.log(response.data)
+      setAllUserFishes(response.data.filter(fish => fish.userId._id === user._id))
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
-    <div className='page-body' style={(seeFisheries || (allUserFishes && allUserFishes[1])) ? {height: "100%"}: {height: "100vh"}}>
-      {user && <div className='user-details'>
-        <div className='details-container'>
-          <div className='user-pic-container'>
-            <img src={user.image} alt="user face" className='user-pic'/>
+    <div
+      className='page-body'
+      style={seeFisheries || (allUserFishes && allUserFishes[1]) ? { height: "100%" } : { height: "100vh" }}
+    >
+      {user && (
+        <div className='user-details'>
+          <div className='details-container'>
+            <div className='user-pic-container'>
+              <img src={user.image} alt='user face' className='user-pic' />
+            </div>
+            <div className='details-links'>
+              <Link to={`/edit/user/`} className='small-buttons edit-btn'>
+                Editar Perfil
+              </Link>
+              <Link to={`/add/fish/`} className='small-buttons'>
+                Adicionar Troféu
+              </Link>
+              <Link to={`/add/fishery/`} className='small-buttons'>
+                Registar Pesca
+              </Link>
+            </div>
           </div>
-          <div className='details-links'>
-            <Link to={`/edit/user/`} className="small-buttons edit-btn">Editar Perfil</Link>
-            <Link to={`/add/fish/`} className="small-buttons">Adicionar Troféu</Link>
-            <Link to={`/add/fishery/`} className="small-buttons">Registar Pesca</Link>
+          <div className='details-box'>
+            <p>
+              <b>{user.name}</b>
+            </p>
+            <p>
+              <b>{user.email}</b>
+            </p>
+            <p>
+              <b>Cartão de Pescador Sustentável:</b> <br></br>
+              {user.sustainableFisherNumber}
+            </p>
           </div>
         </div>
-        <div className='details-box'>
-          <p><b>{user.name}</b></p>
-          <p><b>{user.email}</b></p>
-          <p><b>Cartão de Pescador Sustentável:</b> <br></br>{user.sustainableFisherNumber}</p>
-        </div>
-      </div>}
+      )}
 
       <div className='profile-buttons-box'>
-        <button onClick={toggleSeeFisheries} className={seeFisheries? "buttons switch" : "buttons"}>{seeFisheries? "Esconder Pescas" : "Ver Pescas"}</button>
-        <button onClick={toggleSeeFishes} className={seeFishes? "buttons switch" : "buttons"}>{seeFishes? "Esconder Troféus" : "Ver Troféus"}</button>
-        {loading && <img src={loadingGif} alt="loading"/>}
+        <button onClick={toggleSeeFisheries} className={seeFisheries ? "buttons switch" : "buttons"}>
+          {seeFisheries ? "Esconder Pescas" : "Ver Pescas"}
+        </button>
+        <button onClick={toggleSeeFishes} className={seeFishes ? "buttons switch" : "buttons"}>
+          {seeFishes ? "Esconder Troféus" : "Ver Troféus"}
+        </button>
+        {loading && <img src={loadingGif} alt='loading' />}
       </div>
 
       <div className='card-container'>
-      {allUserFisheries && seeFisheries && allUserFisheries.map(fishery => {
-        return (
-            <div key={fishery._id} className="card-box">
-                <Fishery fishery={fishery}/>
-            </div>
-        )
-      })}
-      </div>
-
-      <div className='card-container'>
-      {allUserFishes && seeFishes && allUserFishes.map(fish => {
-        return (
-          <>
-            <div key={fish._id} className="user-fish-box">
-              <div className='user-fish-img-box'>
-                <img src={fish.image} alt={fish.commonName} style={{height: "10vh"}} className="user-fish-img"/>
+        {allUserFisheries &&
+          seeFisheries &&
+          allUserFisheries.map(fishery => {
+            return (
+              <div key={fishery._id} className='card-box'>
+                <Fishery fishery={fishery} />
               </div>
-              <div className='user-fish-box-details'>
-                <div className='user-fish-box-info'>
-                  <span><b>{fish.commonName} - </b> {fish.weight}kg, {fish.length}cm</span>
+            )
+          })}
+      </div>
+
+      <div className='card-container'>
+        {allUserFishes &&
+          seeFishes &&
+          allUserFishes.map(fish => {
+            return (
+              <>
+                <div key={fish._id} className='user-fish-box'>
+                  <div className='user-fish-img-box'>
+                    <img src={fish.image} alt={fish.commonName} style={{ height: "10vh" }} className='user-fish-img' />
+                  </div>
+                  <div className='user-fish-box-details'>
+                    <div className='user-fish-box-info'>
+                      <span>
+                        <b>{fish.commonName} - </b> {fish.weight}kg, {fish.length}cm
+                      </span>
+                    </div>
+                    <span>{fish.areaFound}</span>
+                    <Link to={`/edit/fish/${fish._id}`} className='edit-fish-btn'>
+                      Editar
+                    </Link>
+                  </div>
                 </div>
-                <span>{fish.areaFound}</span>
-                <Link to={`/edit/fish/${fish._id}`} className="edit-fish-btn">Editar</Link>
-              </div>
-            </div>
-            <hr  style={{border: "1px solid blue", width: "90%", margin: "8px 0px"}}/>
-          </>
-        )
-      })}
+                <hr style={{ border: "1px solid blue", width: "90%", margin: "8px 0px" }} />
+              </>
+            )
+          })}
       </div>
     </div>
   )
